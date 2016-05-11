c41285d936ed03d7b1d6450609dc97dbd99c07ac
putriab <putriabsmkn1@gmail.com>
2016-05-11 17:50:32 +0700

    editt lah

--- controller/pegawai_controller.js
+++ controller/pegawai_controller.js
@@ -1,5 +1,6 @@
 var model = require('../model/pegawai.js');
 var data ={
+	"count" : 0,
 	"status" : "",
 	"detail" : ""
 };
@@ -9,14 +10,73 @@ module.exports = {
 	function(req,res){
 		model.get(function (error,result){
 			if(result.length == 0){
+				data["count"] = result.length;
 				data["status"] = "error";
 				data["detail"] = error;
 			}
 			else{
+				data["count"] = result.length;
 				data["status"] = "success";
 				data["detail"] = result;
 			}
 			res.json(data);
 		});
+	},
+	post: 
+	function (req,res) {
+		model.post(req, function (error,result){
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+
+	},
+
+	put: 
+	function (req,res) {
+		model.put(req, function (error,result){
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+
+	},
+
+	delete: 
+	function (req,res) {
+		model.del(req, function (error,result){
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+
+	}
+
+
+}
 		
 		
