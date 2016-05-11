9bbc776ce208241b175ceba920e916c1ac001f8a
vera dianabela <veradianabela10@gmail.com>
2016-05-11 18:15:11 +0700

    edit cyiiinnn

--- model/pegawai.js
+++ model/pegawai.js
@@ -26,6 +26,23 @@ module.exports = {
 
 	},
 
+		getid: function(callback){
+		var id =req.params.id;
+
+		knex.select().table('tbl_pegawai')
+		.join('tbl_customer', 'tbl_pegawai.id', 'tbl_customer.id_pegawai')
+		.join('tbl_penyewaan', 'tbl_pegawai.id', 'tbl_penyewaan.id_pegawai')
+		.join('tbl_pembayaran', 'tbl_pegawai.id', 'tbl_pembayaran.id_pegawai')
+		.whereRaw('tbl_pegawai = ?', [id])
+		.select(`tbl_pegawai.id`,`nama_pegawai`,`email_pegawai`,`password_pegawai`)
+		.then(function (rows){
+			callback(null, rows);
+		})
+		.catch(function (err){
+			callback(err)
+		});
+},
+
 	post: function(req, callback){
 	var Id = req.params.id;
 	var Nama_pegawai = req.body.nama_pegawai;
