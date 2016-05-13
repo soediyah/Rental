ab461cd0c468c8efcf0561f3e3d59ae6e6b7a83e
soediyah <soediyah@gmail.com>
2016-05-13 14:58:29 +0700

    nambah controller

---
+++ controller/telepon_controller.js
@@ -0,0 +1,101 @@
+var model = require('../model/telepon.js');
+var data ={
+	"count" : 0,
+	"status" : "",
+	"detail" : ""
+};
+
+module.exports = {
+	get: 
+	function(req,res){
+		model.get(function (error,result){
+			console.log(error, result);
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "data kosong";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+	},
+
+	getid:
+	function (req,res){
+		model.getid(req.params.id, function (error,result){
+			if(result.length == 0){
+
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+	},
+	post: 
+	function (req,res) {
+		model.post(req, function (error,result){
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+
+	},
+
+	put: 
+	function (req,res) {
+		model.put(req, function (error,result){
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+
+	},
+
+	delete: 
+	function (req,res) {
+		model.delete(req.params.id, function (error,result){
+			if(result.length == 0){
+				data["count"] = result.length;
+				data["status"] = "error";
+				data["detail"] = error;
+			}
+			else{
+				data["count"] = result.length;
+				data["status"] = "success";
+				data["detail"] = result;
+			}
+			res.json(data);
+		});
+
+	}
+
+
+}
+		
+		
